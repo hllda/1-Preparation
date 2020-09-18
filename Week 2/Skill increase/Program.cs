7c794e9874d0ae890b3174f831792fb880ef472c
hllda <60630245+hllda@users.noreply.github.com>
2020-09-18 15:34:30 +0200

    Week2

---
+++ Week 2/Skill increase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Skill_increase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
