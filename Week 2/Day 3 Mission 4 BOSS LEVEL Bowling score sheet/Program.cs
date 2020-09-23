3daf8fd1a748819b33ffb755563c6c8b064e97d7
hllda <60630245+hllda@users.noreply.github.com>
2020-09-23 09:49:37 +0200

    stuff

--- Week 2/Day 3 Mission 4 BOSS LEVEL Bowling score sheet/Program.cs
+++ Week 2/Day 3 Mission 4 BOSS LEVEL Bowling score sheet/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mission_4
 {
@@ -10,6 +6,73 @@ namespace Mission_4
     {
         static void Main(string[] args)
         {
+            string miss = "-";
+            string spar = "/";
+            string strike = "X";
+            var random = new Random();
+            int scoreSheet = random.Next(0, 11);
+
+            if (scoreSheet == 0)
+            {
+                Console.WriteLine("You decided to not go bowling.");
+            }
+
+            for (int index = 0; index < (scoreSheet); index++)
+            {
+
+                int firstRoll = random.Next(0, 11);
+
+
+
+                if (firstRoll == 0)
+                {
+                    Console.WriteLine($"First roll: {miss}");
+                }
+
+                if (0 < firstRoll && firstRoll < 10)
+                {
+                    Console.WriteLine($"First roll: {firstRoll}");
+                }
+
+
+                if (firstRoll > 9)
+                {
+                    Console.WriteLine($"First roll: {strike}");
+                    Console.WriteLine($"Knocked pins: 10\n");
+                }
+
+                else
+
+                {
+                    int secondRoll = random.Next(0, 11 - firstRoll);
+                    int sum = (firstRoll + secondRoll);
+                    if (secondRoll == 0)
+                    {
+                        Console.WriteLine($"Second roll: {miss}");
+                    }
+
+                    else
+                    {
+
+                        if (sum == 10)
+                        {
+                            Console.WriteLine($"Second roll: {spar}");
+                        }
+
+                        if (0 < sum && sum < 10)
+                        {
+                            Console.WriteLine($"Second roll: {secondRoll}");
+                        }
+
+
+                    }
+                    Console.WriteLine($"Knocked pins: {sum}\n");
+
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
+
+
