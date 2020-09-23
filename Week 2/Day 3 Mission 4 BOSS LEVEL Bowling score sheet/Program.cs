77cf117d54cab1f61961e8aa4e6fb29a0550ac41
hllda <60630245+hllda@users.noreply.github.com>
2020-09-23 10:39:16 +0200

    bowlingsheetmeh

--- Week 2/Day 3 Mission 4 BOSS LEVEL Bowling score sheet/Program.cs
+++ Week 2/Day 3 Mission 4 BOSS LEVEL Bowling score sheet/Program.cs
@@ -12,6 +12,14 @@ namespace Mission_4
             var random = new Random();
             int scoreSheet = random.Next(0, 11);
 
+            string A = "X";
+            string B = "X";
+
+            string line1 = ($"-----+");
+            string line2 = ($"|"); string line2a = ($"|"); string line2b = ($"|");
+            string line3 = ($" ----|");
+            string line4 = ($"     |");
+
             if (scoreSheet == 0)
             {
                 Console.WriteLine("You decided to not go bowling.");
@@ -19,26 +27,23 @@ namespace Mission_4
 
             for (int index = 0; index < (scoreSheet); index++)
             {
-
+                Console.WriteLine($"+{line1}");
                 int firstRoll = random.Next(0, 11);
-
-
 
                 if (firstRoll == 0)
                 {
-                    Console.WriteLine($"First roll: {miss}");
+                    Console.Write($"| {line2}{miss}{line2a}");
                 }
 
                 if (0 < firstRoll && firstRoll < 10)
                 {
-                    Console.WriteLine($"First roll: {firstRoll}");
+                    Console.Write($"| {line2}{firstRoll}{line2a}");
                 }
-
 
                 if (firstRoll > 9)
                 {
-                    Console.WriteLine($"First roll: {strike}");
-                    Console.WriteLine($"Knocked pins: 10\n");
+                    Console.WriteLine($"| {line2}{strike}{line2a}{" "}{line2b}");
+            //       Console.WriteLine($"Knocked pins: 10\n");
                 }
 
                 else
@@ -48,7 +53,7 @@ namespace Mission_4
                     int sum = (firstRoll + secondRoll);
                     if (secondRoll == 0)
                     {
-                        Console.WriteLine($"Second roll: {miss}");
+                        Console.WriteLine($"{miss}{line2b}");
                     }
 
                     else
@@ -56,19 +61,25 @@ namespace Mission_4
 
                         if (sum == 10)
                         {
-                            Console.WriteLine($"Second roll: {spar}");
+                            
+                            Console.WriteLine($"{spar}{line2b}");
                         }
 
                         if (0 < sum && sum < 10)
                         {
-                            Console.WriteLine($"Second roll: {secondRoll}");
+                            Console.WriteLine($"{secondRoll}{line2b}");
                         }
 
 
                     }
-                    Console.WriteLine($"Knocked pins: {sum}\n");
 
+
+           //         Console.WriteLine($"Knocked pins: {sum}\n");
                 }
+                Console.WriteLine($"|{line3}");
+                Console.WriteLine($"|{line4}");
+                Console.WriteLine($"+{line1}\n");
+                
             }
             Console.ReadKey();
         }
