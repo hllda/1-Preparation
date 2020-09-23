4d0563c8ae66b2782a59625cfab9e2438a5c1ae6
hllda <60630245+hllda@users.noreply.github.com>
2020-09-23 15:56:27 +0200

    basilisk

--- Week 2/Day 4 Mission 2 BOSS Battle with the basilisk/Program.cs
+++ Week 2/Day 4 Mission 2 BOSS Battle with the basilisk/Program.cs
@@ -13,7 +13,7 @@ namespace Mission_2
 
             var names = new List<string> { "Joa", "Nell", "Luna", "Kanel" };
             string party = String.Join(", ", names);
-            var intro = $"A party of warriors ({party}) descends into the dungeon.";
+            string intro = $"A party of warriors ({party}) descends into the dungeon.";
 
 
             Console.WriteLine($"{intro}");
@@ -32,48 +32,70 @@ namespace Mission_2
 
             int hit = 0;
 
-            for (int index = 0; index < 1; index++)
+            do
             {
-                foreach (var name in names)
+                foreach (string name in names)
                 {
-                    for (int inde = 0; inde < 1; inde++)
-                    {
+                  for (int inde = 0; inde < 1; inde++)
+                  {
                         int dice = random.Next(1, 5);
                         hit += dice;
-                    }
-
+                  }
                     var attack = ($"{name} hits the basilisk for {hit} damage.");
                     basilisk -= hit;
                     string hpleft = ($"Basilisk has {basilisk} HP left.");
                     hit = 0;
 
+                    Console.WriteLine($"{attack} {hpleft}");
 
-                    if (basilisk < 0)
+                    if (basilisk == 0)
                     {
                         Console.WriteLine($"{attack} Basilisk has 0 HP left."); ;
-                        goto victory;
+                        break;
                     }
-                    Console.WriteLine($"{attack} {hpleft}");
                 }
+                
+                if (basilisk > 0)
+                {
+                Console.WriteLine($"The basilisk uses petrifying gaze on {name}!");
+
+                    for (int inde = 0; inde < 1; inde++)
+                    {
+                        int dice20 = random.Next(1, 5);
+                        roll20 += dice20 + 5;
+                    }
+
+                    if 
+                        
 
 
-            victory:
+                } 
+                    
+
+
+
+            }
+
+
+
+
+              
                 Console.WriteLine($"The basilisk collapses and the heroes celebrate their victory!");
                 goto ending;
 
-            defeat:
+
                 Console.WriteLine($"The party has perished. They will forever stand as statues in the basilisk's great hall of dining.");
                 goto ending;
 
 
-            ending: Console.WriteLine($"The end!");
+                Console.WriteLine($"The end!");
 
                 // 2. After each round of heroes' attacks, the basilisk uses petrifying gaze on a random hero. Display who it is.
                 // 3. The hero has to pass a DC 12 constitution saving throw to survive.Assume they have constitution 5, add a d20 roll, and check if the total is 12 or higher.Display the roll amount and if they succeed to be saved.
                 // 4. If they don't succeed, display that they got turned into stone and remove them from the list of heroes.
                 // 5. If all heroes get turned into stone before they defeat the basilisk, the game is over.Display a bad ending narrative.
 
-            }
+            
         }
     }
 }
