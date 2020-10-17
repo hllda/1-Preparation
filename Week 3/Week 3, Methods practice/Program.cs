0bd7678b72142e9e7030b92d6040f37546c2ec2e
hllda <60630245+hllda@users.noreply.github.com>
2020-10-17 18:51:49 +0200

    yay method practise shit is done

--- Week 3/Week 3, Methods practice/Program.cs
+++ Week 3/Week 3, Methods practice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
@@ -106,33 +107,206 @@ namespace Week_3__Methods_practice
 
 
         //10
-
+        static string CreateRandomIntegers(int minimum, int maximum, int count)
+        {
+        List<int> randomInts = new List<int> {};
+        var random = new Random();
+        
+        for (int i = 0; i < count; i++)
+        {
+        int minmax = random.Next(minimum,maximum+1);
+        randomInts.Add(minmax);
+        }
+        return String.Join(", ", randomInts);
+        }
 
 
         //11
+        static string Indent(int indentsam, string words)
+        {
+        string indents = "";
+        for (int i = 0; i < indentsam; i++)
+        {
+        indents = $"{indents} ";
+        }
+        string indented = $"{indents}{words}";
+        return $"{indented}";
+        }
 
         //12
+        static string CreateFullNames(List <string> firstname, List <string> lastname)
+        {
+        List<string> names = new List<string>{};
+
+        for (int name = 0; name < firstname.Count; name++)
+        {
+        names.Add(string.Concat(firstname[name], " ", lastname[name]));
+        }
+        return String.Join(", ", names);
+        }
+
 
         //13
+        static string ZipStrings(List <string> nameones, List <string> nametwos)
+        {
+        List<string> names = new List<string>{};
+        if (nametwos.Count < nameones.Count)
+        { 
+            for (int name = 0; name < nameones.Count; name++)
+            {
+            names.Add(nameones[name]);
+            if (nametwos.Count > name)
+            {
+            names.Add(nametwos[name]);
+            }
+            
+            }     
+        }
+
+        else if (nameones.Count < nametwos.Count)
+        {
+            for (int name = 0; name < nametwos.Count; name++)
+            {
+           
+
+            if (nameones.Count > name)
+            {
+            names.Add(nameones[name]);
+            }
+            names.Add(nametwos[name]);
+            }
+        }
+
+        else
+        {
+            for (int name = 0; name < nameones.Count; name++)
+            {
+            names.Add(nameones[name]);
+            names.Add(nametwos[name]);
+            }
+        }
+
+        return String.Join(", ", names);
+        }
+
+
 
         //14
+        static int CountStrings(List <string> weirdos, string searchValue)
+        {
+        int searchString = weirdos.Where(x => x.Equals(searchValue)).Count();
+                // ($"{searchValue}");
+        return searchString;
+        }
 
-
-
+        // I know I didn't use recursion, but it didn't feel necessary, if it's more efficient in any of the cases please show me :D
         //1
+        static int Power(int a, int b)
+        {
+            int sum = a;
+            for (int i = 0; i < b-1; i++)
+            {
+            sum *= a;
+            }
 
+        return sum;
+        }
+        
+        
         //2
-
+        static string Factorial(int n)
+        {
+        List<int> nomnom = new List<int>{};
+        int sum = 1;
+        for (int i = 1; i < n+1; i++)
+        {
+        sum = sum * i;
+        nomnom.Add(sum);
+        }
+        string sums = String.Join(", ", nomnom);
+        return sums;
+        }
+        
+        
         //3
-
+        static int Fibonacci(int n)
+        {
+        int sum = n; 
+        for (int i = 1; i < n; i++)
+        {
+        sum = (Fibonacci(n - 1) + Fibonacci(n - 2));
+        }
+        return sum;
+        }
+        
+        
         //4
+        static string CreateListOfDigits(int digits)
+        {
+        if (digits == 0)
+        {
+        return "0";
+        }
+        List<int> digi = new List<int>{};
+        for (int i = 0; i < digits-1; i++)
+        {
+        int dig = digits % 10;
+        digi.Add(dig);
+        digits = (digits - dig) / 10; 
+        }
+        digi.Add(digits);
+        digi.Sort();
+        return String.Join(", ", digi);
+        }
+
 
         //5
+        static string ConvertToBinary(string deciText)
+        {
+        int deci = Int32.Parse(deciText);
+        string bin = Convert.ToString(deci, 2);
+        
+        return bin;
+        }
 
+        
         //6
+        static int SmallestIntegers(List<int> smallest)
+        {
+        List<int> smallestfirst = new List<int>(smallest);
+        int cunt = smallest.Count;
+        smallestfirst.RemoveRange(cunt/2, cunt/2);
+        smallestfirst.Sort();
+        
+        List<int> smallestsecond = new List<int>(smallest);
+        smallestsecond.RemoveRange(0, cunt/2);
+        smallestsecond.Sort();
+        
+        List<int> smallestVSsmallest = new List<int>{smallestfirst[0], smallestsecond[0]};
+        smallestVSsmallest.Sort();
+
+        return smallestVSsmallest[0];
+        }
+
 
         //7
+        static string SelectionSort(List<int> inefficientSort)
+        {
 
+        for (int j = 0; j < inefficientSort.Count; j++)
+        { 
+            for (int i = 0; i < inefficientSort.Count; i++)
+            {   
+                if (inefficientSort[i] > inefficientSort[j])
+                {
+                int temporary = inefficientSort[j];
+                inefficientSort[j] = inefficientSort[i];
+                inefficientSort[i] = temporary;
+                }
+            }
+        }
+        return string.Join(", ", inefficientSort);
+        }
 
 
 
@@ -192,36 +366,75 @@ namespace Week_3__Methods_practice
         List<int> listtwo = new List<int> {1, 2, 5, 7,};
 
         Console.WriteLine(JoinIntegers(listone, listtwo));
-
+        Console.WriteLine();
 
 
         //10
+        var random = new Random();
+        int cou = random.Next(10,21);
+        Console.WriteLine(CreateRandomIntegers(1, 5, cou));
+        Console.WriteLine();
 
+            
         //11
+        int indentsam = 6;
+        string words = $"<- Here are {indentsam} spaces!";
+        Console.WriteLine(Indent(indentsam, words));
+        Console.WriteLine();
 
         //12
+        List<string> firstname = new List<string> {"Lara", "Duke", "Sonic"};
+        List<string> lastname = new List<string> {"Croft", "Nukem", "the Hedgehog"};
+        Console.WriteLine(CreateFullNames(firstname, lastname));
+        Console.WriteLine();  
+        
 
         //13
+        List<string> nameones = new List<string> {"Mario", "Luigi", "Peach", "Bowser", "Yoshi"};
+        List<string> nametwos= new List<string> {"Link", "Zelda", "Ganondorf", "Hilda", "Tingle", "Zant", "Midna"};
+        Console.WriteLine(ZipStrings(nameones, nametwos));
+        Console.WriteLine();  
 
         //14
-
+        List<string> weirdos = new List<string> {"ADD", "DEL", "INC", "ADD", "JMP", "SUB", "DEC"};
+        string searchValue = "ADD";
+        Console.WriteLine($"The word {searchValue} occurs in the list {CountStrings(weirdos, searchValue)} times");
+        Console.WriteLine("\n");
 
 
         //1
+        int a = 2;
+        int b = 3;
+        Console.WriteLine(Power(a, b));
+        Console.WriteLine();
 
         //2
+        int n = 5;
+        Console.WriteLine(Factorial(n)); 
+        Console.WriteLine();
 
         //3
+        Console.WriteLine($"Fibonacci number on place {n} is {Fibonacci(n)}");  
+        Console.WriteLine();
 
         //4
+        int digits = 582399;
+        Console.WriteLine(CreateListOfDigits(digits));
+        Console.WriteLine();
 
         //5
+        string deciText = "255";
+        Console.WriteLine($"The decimal number {deciText} is written as {ConvertToBinary(deciText)} in binary.");   
+        Console.WriteLine();
 
         //6
+        List<int> smallest = new List<int> {16, 6, 69, 42, 14, 24, 32};
+        Console.WriteLine(SmallestIntegers(smallest));
+        Console.WriteLine();
 
         //7
-
-
+        List<int> inefficientSort = new List<int> {16, 6, 69, 42, 14, 24, 32};
+        Console.WriteLine(SelectionSort(inefficientSort));
         }
     }
 }
