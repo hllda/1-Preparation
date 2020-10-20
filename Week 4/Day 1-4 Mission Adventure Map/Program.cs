b9e1b09f0c028e3b38a9717d6bda58cd91a54bd5
hllda <60630245+hllda@users.noreply.github.com>
2020-10-20 21:03:33 +0200

    XX<Z

--- Week 4/Day 1-4 Mission Adventure Map/Program.cs
+++ Week 4/Day 1-4 Mission Adventure Map/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Day_1_4_Mission_Adventure_Map
 {
@@ -9,87 +11,148 @@ namespace Day_1_4_Mission_Adventure_Map
         // BORDER - this is where the border is drawn.
         static void Border(int width, int height)
         {
-            Console.WriteLine($"+{"+".PadLeft(width, '-')}");
-            string title = "ADVENTURE MAP";
-            Console.WriteLine($"|{title.PadLeft(width/2+title.Length/2-1,' ')}{"|".PadLeft(width/2-title.Length/2+1,' ')}");
-            for (int i = 0; i < height; i++)
-            {
-            Console.WriteLine($"|{"|".PadLeft(width, ' ')}");
-            }
-            Console.WriteLine($"+{"+".PadLeft(width, '-')}");
+
         }   
         
-
-   
         static void River(int width, int height)
         {
-        Console.SetCursorPosition(1, 1);
         
         }
 
         static void Road(int width, int height)
         {
-        Console.SetCursorPosition(1, 1);
+
         }
 
         static void Bridge(int width, int height)
         {
-        Console.SetCursorPosition(1, 1);
+
         }   
 
         static void Path(int width, int height)
         {
-        Console.SetCursorPosition(1, 1);
+
         }   
 
         static void Forest(int width, int height)
         {
-        Console.SetCursorPosition(1, 1);
+
         }   
 
         static void DrawMap(int width, int height)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < height+1; y++)
             {
                 for (int x = 0; x < width+1; x++)
-                {
-            
-                    if (y == 0 && x == 0)
+                {   
+
+
+                    // DRAW TITLE?
+                    string title = "ADVENTURE MAP";
+                    if (y == 1 && x == width/2-title.Length/2-1)
                     {
-                    Console.Write("+");
+                    Console.Write($"{title}");
+                    x += title.Length-1;
+                    continue;
                     }
 
-                    if (y == 0 && x != 0)
+                    // DRAW BORDER?
+                    if (y == 0 || x == 0 || y == height || x == width)
                     {
-                    Console.Write("-");
+                        if (y == 0 && x == 0 ||y == height && x == 0)
+                        {
+                        Console.Write("+");
+                        continue;
+                        }
+
+                        if (y == 0 && x != 0 && x != width || y == height && x != 0 && x != width)
+                        {
+                        Console.Write("-");
+                        continue;
+                        }
+
+                        if (y == 0 && x == width || y == height && x == width)
+                        {
+                        Console.WriteLine("+");
+                        continue;
+                        }
+
+                        if (x == 0 && y != 0 && y != height)
+                        {
+                        Console.Write("|");
+                        continue;
+                        }
+
+                        if (x == width && y != 0 && y != height)
+                        {
+                        Console.WriteLine("|");
+                        continue;
+                        }
                     }
 
-                    if (y == 0 && x == width)
+                    // DRAW TREE?
+                    var random = new Random();
+                    var treelist = new List<int>{ };
+                    var tree = "A^¤@";
+                    if (x < width/4)
                     {
-                    Console.WriteLine("+");
+                      
                     }
 
 
-                    if (y != 0 && x == 0)
-                    {
-                    Console.Write("|");
-                    }
 
-                    if (y != 0 && x != width && y != height)
-                    {
-                    Console.Write(" ");
-                    }
 
-                    if (y != 0 || y != height && x == width)
-                    {
-                    Console.WriteLine("|");
-                    }
-
-                    
                 }
+
+
+
+
+                    Console.Write(" ");
+
+                 
+
+
+
             }
-        }   
+        }
+      
         
+
+    
+         static void Matrix(string[] args)
+         {
+            var random = new Random();
+            List<int> treeList = new List<int>{};
+            var symbols = "!@#$%^&*()_+-=[];',.\\/~{}:|<>?";
+
+            for (int i = 0; i < 10; i++) 
+            {
+            treeList.Add(random.Next(0, 80));
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            while (true)
+            {
+                for (int x = 0; x < 80; x++)
+                {
+                    Console.Write(treeList.Contains(x) ? symbols[random.Next(symbols.Length)] : ' ');
+                }
+
+                Console.WriteLine();
+                Thread.Sleep(100);
+
+                if (random.Next(3) == 0) 
+                { 
+                treeList.RemoveAt(random.Next(treeList.Count-1));
+                }
+                
+                if (random.Next(3) == 0)
+                { 
+                treeList.Add(random.Next(0, 80));
+                }
+            }   
+         }
 
         static void Main(string[] args)
         {  
@@ -98,12 +161,12 @@ namespace Day_1_4_Mission_Adventure_Map
         DrawMap(width, height);
         }
 
-
-
     }
+
+}
 
         
 
 
-}
+
 
