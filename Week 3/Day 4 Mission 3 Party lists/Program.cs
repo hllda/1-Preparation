32fb5e00ee11c95697b07041ecc22cce36f19374
hllda <60630245+hllda@users.noreply.github.com>
2020-11-28 14:52:52 +0100

    ghdg

--- Week 3/Day 4 Mission 3 Party lists/Program.cs
+++ Week 3/Day 4 Mission 3 Party lists/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace Day_4_Mission_3_Party_lists
 {
@@ -8,7 +9,6 @@ namespace Day_4_Mission_3_Party_lists
     {      
         static int Factorial(int n)
         {   
-
             if (n == 0)
             {
             return 1;
@@ -17,75 +17,78 @@ namespace Day_4_Mission_3_Party_lists
             {
             return n * Factorial(n - 1);
             }
+        }
 
-        } 
+        static void Swap(List<string> names, int l, int r)
+        {    
+            string temp = names[l];
+            names[l] = names[r];
+            names[r] = temp;
+            return;
+        }
 
-        static string WriteAllPermutations(List<string> names, int n)
-        {
-            
-            
+        static void WriteAllPermutations(List<string> names, int l, int r)
+        {  
             if (names.Count == 0)
             {
-            return "";
+            return;
             }
 
             if (names.Count == 1)
             {
-            return names[0];
+            return;
+            }
+
+            if (names.Count == 2)
+            {
+            Swap(names, l, r);
+            return;
             }
 
             else
             {
+                if (l == r) return;
 
-            for (int i = 1; i < n; i++)
+                else
                 {
-                string name0 = names.Add(string.Concat(names[0], " ", names[1]));
+                    for (int i = l; i <= r; i++)
+                    {
+                        Swap(names, l, i);
+                        WriteAllPermutations(names, l+1, r);
+                        Swap(names, r, i);
+                    }
+                    return;
                 }
-            
-                       
-            
-
-
-
-
-            }
-
-            var Random = new Random();
-            for (int i = 0; names.Count-1 > i; i++)
-            {
-                int j = Random.Next(0, names.Count);
-                string hold = names[i];
-                names[i] = names[j];
-                names[j] = hold;
-            }
-            
-            
-            int timesPerName = Factorial(names.Count)/names.Count;
-            return "";
-
+            } 
         }
 
-
-
         static void Main(string[] args)
-        {
-
-            var names = new List<string> {"Hilda", "Luna", "Kanel", "Joao"};
+        {  
+            // NAMES
+            var names = new List<string> {"Hilda", "Luna", "Kanel"};
             names.Sort();
+            int n = names.Count;
+            
+            // WRITE OUT JOINED
             string joined = String.Join(", ", names);
             Console.WriteLine($"Signed-up participants: {joined}\n");
             Console.WriteLine("Starting orders:");
+            
+            // WRITE ALL PERMUTATIONS
+            
 
-            for (int num = 1; num < Factorial(names.Count)+1; num++)
+            for (int i = 1; i < Factorial(names.Count) + 1; i++)
             {
-            Console.Write($"{num}. ");
-            joined = string.Join(", ", names);
-            WriteAllPermutations(names);
-            Console.WriteLine($"{joined}");
-            }
-            var Random = new Random();
-            int n = Random.Next(0, 11);
+                Console.Write($"[{i}.");
 
+                if (i < 10 && Factorial(names.Count) > 10) Console.Write(" ");
+            
+                Console.Write("] ");
+               
+                joined = string.Join(", ", names);
+                WriteAllPermutations(names, 0, n-1);
+                Console.WriteLine($"{joined}");
+            }
         }
     }
 }
