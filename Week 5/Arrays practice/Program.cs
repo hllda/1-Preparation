6fac48a09b31ca8606430f6e8ce33c5bdb334ee1
hllda <60630245+hllda@users.noreply.github.com>
2020-12-02 00:18:35 +0100

    asd

--- Week 5/Arrays practice/Program.cs
+++ Week 5/Arrays practice/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
 namespace Arrays_practice
 {
     class Program
     {
         static void Main(string[] args)
         {
+        var Random = new Random();
+        
         // PART 1
         Console.WriteLine("PART 1");
         // 1
@@ -22,29 +23,26 @@ namespace Arrays_practice
 
         // 2
         Console.WriteLine(2);
-        string[] monthdays = new string[31];
- 
-        Console.Write("Days this month are: ");
-        int cycle = 1;    
-        for (int i = 0; i < 31; i++)
-            {
-            if (cycle > 6) cycle = 0;
-            monthdays[i] = weekdays[cycle];
-            cycle++;
-            }
+        var today = DateTime.Today;
+        var month = new DateTime(today.Year, today.Month, 1);
 
-        for (int i = 1; i < 31+1; i++)
-            {
-            Console.Write($"{i}: {monthdays[i-1]}");
-            if (i < 31) Console.Write(", ");
+        int daysInMonth = DateTime.DaysInMonth(today.Year, today.Month);
+        string[] monthdays = new string[daysInMonth];
+        
+        Console.WriteLine("Days this month are: "); 
+        string weekday = month.ToString("dddd");
+
+        for (int i = 0; i < daysInMonth; i++)
+            {    
+            monthdays[i] = month.ToString("ddd");
+            Console.WriteLine($"{i+1}: {monthdays[i]}");
+            month = month.AddDays(1);
             }
         Console.WriteLine($"\n\n");
-          
+  
 
-        
         // 3
         Console.WriteLine(3);
-        var Random = new Random();
         int arrayLength = Random.Next(5,11);
         
         double[] arrayOfDoubles = new double[arrayLength];
@@ -52,52 +50,139 @@ namespace Arrays_practice
             {
             double randomDouble = Random.Next(1,11);
             arrayOfDoubles[i] = randomDouble;
-            }
-            Console.Write($"{arrayLength} random numbers are: ");
-            
-        for (int i = 0; i < arrayLength; i++)
-            {
-            Console.Write($"{arrayOfDoubles[i]}");
-            if (i < arrayLength-1) Console.Write(", ");
             } 
+        Console.WriteLine($"{arrayLength} random numbers are: {string.Join(", ", arrayOfDoubles)}");
         Console.WriteLine($"\n\n");   
 
 
         // 4
         Console.WriteLine(4);
-        double[] interpolated = new double[arrayLength];
-
-
-
-
-
-        Console.WriteLine();
-
+        var interpolated = new double[arrayLength * 2 - 1];
+        interpolated[0] = arrayOfDoubles[0];
+        for (int i = 1; i < arrayLength; i++)
+            {
+            interpolated[i * 2] = (arrayOfDoubles[i]);
+            interpolated[i * 2 - 1] = (arrayOfDoubles[i] + arrayOfDoubles[i-1]) / 2;
+            }
+        Console.WriteLine($"Interpolated numbers are: {string.Join(", ", interpolated)}");
         Console.WriteLine($"\n\n");
+
+
 
         // PART 2
         Console.WriteLine("PART 2");
         // 1
         Console.WriteLine(1);
-        string[] b = {};
-        Console.WriteLine();
+
+        int hor = Random.Next(2,6);
+        int ver = Random.Next(2,6);
+        int[,] array2D = new int[hor, ver];
+        for (int y = 0; y < ver; y++)
+        {
+            for (int x = 0; x < hor; x++)
+            {
+            int fil = Random.Next(0,10);
+            array2D[x, y] = fil;
+            }
+        }
+
+        for (int y = 0; y < ver; y++)
+        {
+            for (int x = 0; x < hor; x++)
+            {
+            Console.Write(array2D[x, y]);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine($"\n\n");
+
 
         // 2
+        int[,] array2Dtransposed = new int[ver, hor];
         Console.WriteLine(2);
-        Console.WriteLine();
+        for (int y = 0; y < hor; y++)
+        {
+            for (int x = 0; x < ver; x++)
+            {
+            array2Dtransposed[x, y] = array2D[y, x];
+            }
+        }
+
+        for (int y = 0; y < hor; y++) 
+        {
+            for (int x = 0; x < ver; x++)   
+            {
+            Console.Write(array2Dtransposed[x, y]);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine($"\n\n");
+
         
         // 3
         Console.WriteLine(3);
-        Console.WriteLine();
+        int[,] tenTable = new int[11, 11];
+        for (int y = 1; y <= 10; y++)
+        {
+            for (int x = 1; x <= 10; x++)
+            {
+            tenTable[x, y] = x * y;
+            }
+        }
+
+        for (int y = 1; y <= 10; y++) 
+        {
+            for (int x = 1; x <= 10; x++)   
+            {
+            Console.Write($"{tenTable[x, y]} ");
+            if (x * y < 10) Console.Write(" ");   
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine($"\n\n");
 
         // 4
         Console.WriteLine(4);
+        int[,] chess = new int[8, 8];
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+            chess[x, y] = 0;
+            }
+        }
+
+        for (int y = 0; y < 8; y++) 
+        {
+            for (int x = 0; x < 8; x++)   
+            {
+            Console.Write($"{chess[x, y]} ");
+            }
+            Console.WriteLine();
+        }
+
         Console.WriteLine();
 
         // 5
         Console.WriteLine(5);
-        Console.WriteLine();
+        char[,] tic = new char[3, 3];
+        for (int y = 0; y < 3; y++)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+            tic[x, y] ='#';
+            }
+        }
 
+        for (int y = 0; y < 3; y++) 
+        {
+            for (int x = 0; x < 3; x++)   
+            {
+            Console.Write($"{tic[x, y]} ");
+            }
+            Console.WriteLine();
+        }
         Console.WriteLine($"\n\n");
 
         // PART 3
