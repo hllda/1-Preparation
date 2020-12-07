75f7ded8f5004d239ef355494efbac840a5147e3
hllda <60630245+hllda@users.noreply.github.com>
2020-12-07 09:54:55 +0100

    jjkhkj

--- Week 5/Arrays practice/Program.cs
+++ Week 5/Arrays practice/Program.cs
@@ -4,8 +4,14 @@ namespace Arrays_practice
 {
     class Program
     {
-        static void tac(char[,] tic, char current, int empty)
+        static int tac(char[,] tic,  int empty)
         {
+            var Random = new Random();    
+            int win = 69;
+            int randomC = Random.Next(0,2);
+            char current = 'N';    
+            if (randomC == 0) current = 'O';
+            if (randomC == 1) current = 'X';
 
             bool one = false;
             bool two = false;
@@ -16,130 +22,141 @@ namespace Arrays_practice
             bool seven = false;
             bool eight = false;
             bool nine = false;
-
-            var Random = new Random();
-            
-            
-            for (int i = 0; i < 9; i++)
+    
+            tryagain:
+            for (int i = 0; i < 99; i++)
             { 
                 int randomX = Random.Next(0,3);
                 int randomY = Random.Next(0,3);
   
-                if(randomX == 0 && randomY == 0 && one == false)
+                if((randomX == 0 && randomY == 0) && one == false)
                 {
                     tic[0,0] = current;
                     one = true;
                     empty--;
                 }
 
-                if(randomX == 0 && randomY == 1 && two == false)
+                else if((randomX == 0 && randomY == 1) && two == false)
                 {
                     tic[0,1] = current;
                     two = true;
                     empty--;
                 }
 
-                if(randomX == 0 && randomY == 2 && three == false)
+                else if((randomX == 0 && randomY == 2) && three == false)
                 {
                     tic[0,2] = current;
                     three = true;
                     empty--;
                 }
 
-                if(randomX == 1 && randomY == 0 && four == false)
+                else if((randomX == 1 && randomY == 0) && four == false)
                 {
                     tic[1,0] = current;
                     four = true;
                     empty--;
                 }
 
-                if(randomX == 1 && randomY == 1 && five == false)
+                else if((randomX == 1 && randomY == 1) && five == false)
                 {
                     tic[1,1] = current;
                     five = true;
                     empty--;
                 }
            
-                if(randomX == 1 && randomY == 2 && six == false)
+                else if((randomX == 1 && randomY == 2) && six == false)
                 {
                     tic[1,2] = current;
                     six = true;
                     empty--;
                 }
 
-                if(randomX == 2 && randomY == 0 && seven == false)
+                else if((randomX == 2 && randomY == 0) && seven == false)
                 {
                     tic[2,0] = current;
                     seven = true;
                     empty--;
                 }
 
-                if(randomX == 2 && randomY == 1 && eight == false)
+                else if((randomX == 2 && randomY == 1) && eight == false)
                 {
                     tic[2,1] = current;
                     eight = true;
                     empty--;
                 }
 
-                if(randomX == 2 && randomY == 2 && nine == false)
+                else if((randomX == 2 && randomY == 2) && nine == false)
                 {
                     tic[2,2] = current;
                     nine = true;
                     empty--;
                 }
-         
-
-                if 
-                // VERTICAL
-                  ((tic[0,0] == 'X' && tic[0,1] == 'X' && tic[0,2] == 'X') 
-                || (tic[1,0] == 'X' && tic[1,1] == 'X' && tic[1,2] == 'X')
-                || (tic[2,0] == 'X' && tic[2,1] == 'X' && tic[2,2] == 'X')
-
-                // HORIZONTAL
-                || (tic[0,0] == 'X' && tic[1,0] == 'X' && tic[2,0] == 'X') 
-                || (tic[0,1] == 'X' && tic[1,1] == 'X' && tic[2,1] == 'X')
-                || (tic[0,2] == 'X' && tic[1,2] == 'X' && tic[2,2] == 'X')
-
-                // DIAGONAL
-                || (tic[0,0] == 'X' && tic[1,1] == 'X' && tic[2,2] == 'X') 
-                || (tic[0,2] == 'X' && tic[1,1] == 'X' && tic[2,0] == 'X'))
-                { 
-                return;
-                }
-
-                if
-                // VERTICAL
-                  ((tic[0,0] == 'O' && tic[0,1] == 'O' && tic[0,2] == 'O') 
-                || (tic[1,0] == 'O' && tic[1,1] == 'O' && tic[1,2] == 'O')
-                || (tic[2,0] == 'O' && tic[2,1] == 'O' && tic[2,2] == 'O')
-
-                // HORIZONTAL
-                || (tic[0,0] == 'O' && tic[1,0] == 'O' && tic[2,0] == 'O') 
-                || (tic[0,1] == 'O' && tic[1,1] == 'O' && tic[2,1] == 'O')
-                || (tic[0,2] == 'O' && tic[1,2] == 'O' && tic[2,2] == 'O')
-
-                // DIAGONAL
-                || (tic[0,0] == 'O' && tic[1,1] == 'O' && tic[2,2] == 'O') 
-                || (tic[0,2] == 'O' && tic[1,1] == 'O' && tic[2,0] == 'O'))
-                { 
-                return;
-                }
-
-            
-                
-                if (current == 'O') current = 'X';
-                if (current == 'X') current = 'O';
-
-
-                if (one == true && two == true && three == true && four == true && five == true && six == true && seven == true && eight == true && nine == true)
+      
+                else
                 {
-                return;
+                goto tryagain;
+                }
+                // VERTICAL
+                if
+                ((tic[0,0] == 'O' && tic[1,0] == 'O' && tic[2,0] == 'O') || (tic[0,1] == 'O' && tic[1,1] == 'O' && tic[2,1] == 'O') || (tic[0,2] == 'O' && tic[1,2] == 'O' && tic[2,2] == 'O'))
+                {
+                return
+                win = 1;
                 }
 
+                // DIAGONAL
+                if ((tic[0,0] == 'O' && tic[1,1] == 'O' && tic[2,2] == 'O') || (tic[0,2] == 'O' && tic[1,1] == 'O' && tic[2,0] == 'O'))
+                {
+                return 
+                win = 11;
+                }
+
+                // HORIZONTAL
+                if ((tic[0,0] == 'O' && tic[0,1] == 'O' && tic[0,2] == 'O') || (tic[1,0] == 'O' && tic[1,1] == 'O' && tic[1,2] == 'O') || (tic[2,0] == 'O' && tic[2,1] == 'O' && tic[2,2] == 'O'))
+                {
+                return 
+                win = 21;
+                }
+
+                // VERTICAL
+                if ((tic[0,0] == 'X' && tic[1,0] == 'X' && tic[2,0] == 'X') || (tic[0,1] == 'X' && tic[1,1] == 'X' && tic[2,1] == 'X') || (tic[0,2] == 'X' && tic[1,2] == 'X' && tic[2,2] == 'X'))
+                {
+                return
+                win = 2;
+                }
+
+                // DIAGONAL
+                if ((tic[0,0] == 'X' && tic[1,1] == 'X' && tic[2,2] == 'X') || (tic[0,2] == 'X' && tic[1,1] == 'X' && tic[2,0] == 'X'))
+                {
+                return
+                win = 12;
+                }
+
+                // HORIZONTAL
+                if ((tic[0,0] == 'X' && tic[0,1] == 'X' && tic[0,2] == 'X') || (tic[1,0] == 'X' && tic[1,1] == 'X' && tic[1,2] == 'X') || (tic[2,0] == 'X' && tic[2,1] == 'X' && tic[2,2] == 'X'))
+                {
+                return win = 22;
+                }
+
+
+                if ((one == true) && (two == true) && (three == true) && (four == true) && (five == true) && (six == true) && (seven == true) && (eight == true) && (nine == true) && (empty == 0))
+                {
+                return win = 0;
+                }
+                
+                if(current == 'O')
+                {
+                current = 'X';
+                }
+
+                else if(current == 'X')
+                {
+                current = 'O';
+                }
+                
 
             }
-
-          
+        return win = 0;
         }
         
 
@@ -340,24 +357,31 @@ namespace Arrays_practice
             // 5
             Console.WriteLine(5);
 
-            int randomC = Random.Next(0,2);
-            char current = ' ';
             int empty = 9;
-            int win = 1;
-            if (randomC == 0) current = 'O';
-            if (randomC == 1) current = 'X';
+
             char[,] tic = new char[3, 3];
-            
+
             for (int y = 0; y < 3; y++)
-            {
+            {   
+                char current = ' ';
                 for (int x = 0; x < 3; x++)
                 {
                 tic[x, y] ='*';
                 }
             }
            
-            tac(tic, current, empty);  
-  
+            int win = tac(tic, empty);  
+
+            string direction = "error";
+            if (win == 1 || win == 2) direction = "vertically";
+            if (win == 11 || win == 12) direction = "diagonally";
+            if (win == 21 || win == 22) direction = "horizontally";
+
+            char winner = 'n';
+            if (win == 1 || win == 11 || win == 21) winner = 'O';
+            if (win == 2 || win == 12 || win == 22) winner = 'X';
+            
+
             for (int x = 0; x < 3; x++) 
             {
                 for (int y = 0; y < 3; y++)   
@@ -367,15 +391,22 @@ namespace Arrays_practice
                 Console.WriteLine();
             }
 
-            Console.WriteLine($"{current} won!");
+            if (win == 0) Console.WriteLine($"It's a TIE!");
+            else Console.WriteLine($"{winner} won {direction}!");
 
             Console.WriteLine($"\n\n");
+
+
 
 
             // PART 3
             Console.WriteLine("PART 3");
             // 1
             Console.WriteLine(1);
+
+
+        
+            int[,] wowie = new int[3, 3];
             string[] a = {};
             Console.WriteLine();
         
