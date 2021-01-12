431329445e73a3d326a3d56085a03a3c63d7ad36
hllda <60630245+hllda@users.noreply.github.com>
2021-01-13 00:02:22 +0100

    asdasdas

---
+++ Week 5/Day 2 Mission 1 Full bowling score sheet 2/Program.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_2_Mission_1_Full_bowling_score_sheet
+{
+    class Program
+    {
+        static void FirstRoll(string[,] score, int pinsDown, int rounds, int firstRoll, List<int> standingPins)
+        {
+            firstRoll = pinsDown;
+            if(standingPins.Count == 0)
+                score[rounds, 0] = "X";
+
+            else if(standingPins.Count == 10)
+                score[rounds, 0] = "-";
+
+            else
+                score[rounds, 0] = (10 - standingPins.Count).ToString();
+
+
+
+
+        }
+
+        static void SecondRoll(string[,] score, int pinsDown, int rounds, int firstRoll, int secondRoll, List<int> standingPins, List<int> standingPinsCopy)
+        {
+            secondRoll = firstRoll - pinsDown;
+            if(standingPins.Count == 0)
+                score[rounds, 1] = "/";
+
+            else if(standingPins.Count == standingPinsCopy.Count)
+                score[rounds, 1] = "-";
+
+            else
+                score[rounds, 1] = (secondRoll).ToString();
+        }
+
+        static void ScoreSheet(string[,] score)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐");
+
+            Console.WriteLine($"│ │{score[0, 0]}│{score[0, 1]}│ │{score[1, 0]}│{score[1, 1]}│ │{score[2, 0]}│{score[2, 1]}" +
+                              $"│ │{score[3, 0]}│{score[3, 1]}│ │{score[4, 0]}│{score[4, 1]}│ │{score[5, 0]}│{score[5, 1]}" +
+                              $"│ │{score[6, 0]}│{score[6, 1]}│ │{score[7, 0]}│{score[7, 1]}│ │{score[8, 0]}│{score[8, 1]}" +
+                              $"│ │{score[9, 0]}│{score[9, 1]}│{score[9, 2]}│");
+
+            Console.WriteLine("│ └─┴─┤ └─┴─┤ └─┴─┤ └─┴─┤ └─┴─┤ └─┴─┤ └─┴─┤ └─┴─┤ └─┴─┤ └─┴─┴─┤");
+            Console.WriteLine("│     │     │     │     │     │     │     │     │     │       │");
+            Console.Write("└─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴───────┘");
+        }
+
+        static void KnockPinOnPath(char[] pins, int chosenLane, int pinsDown, List<int> standingPins)
+        {
+            if(chosenLane == 1)
+            {
+                pins[1] = ' ';
+                standingPins.Remove(1);
+            }
+
+            if(chosenLane == 2)
+            {
+                if(pins[2] == 'O')
+                {
+                    pins[2] = ' ';
+                    standingPins.Remove(2);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+            }
+
+            if(chosenLane == 3)
+            {
+                if(pins[3] == 'O')
+                {
+                    pins[3] = ' ';
+                    standingPins.Remove(3);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+
+                else if(pins[8] == 'O')
+                {
+                    pins[8] = ' ';
+                    standingPins.Remove(8);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+            }
+
+            if(chosenLane == 4)
+            {
+                if(pins[4] == 'O')
+                {
+                    pins[4] = ' ';
+                    standingPins.Remove(4);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+
+                else if(pins[9] == 'O')
+                {
+                    pins[9] = ' ';
+                    standingPins.Remove(9);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+            }
+
+            if(chosenLane == 5)
+            {
+                if(pins[5] == 'O')
+                {
+                    pins[5] = ' ';
+                    standingPins.Remove(5);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+
+                else if(pins[0] == 'O')
+                {
+                    pins[0] = ' ';
+                    standingPins.Remove(0);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+            }
+
+            if(chosenLane == 6)
+            {
+                if(pins[6] == 'O')
+                {
+                    pins[6] = ' ';
+                    standingPins.Remove(6);
+                    Path(pins, chosenLane, pinsDown, standingPins);
+                }
+            }
+
+            if(chosenLane == 7)
+            {
+                pins[7] = ' ';
+                standingPins.Remove(7);
+            }
+        }
+
+        static void Path(char[] pins, int chosenLane, int pinsDown, List<int> standingPins)
+        {
+            var Random = new Random();
+            int path = Random.Next(0, 101);
+
+            if(path < 33)
+            {
+                chosenLane++;
+            }
+
+            if(34 < path && path > 66)
+            {
+                chosenLane--;
+            }
+
+            KnockPinOnPath(pins, chosenLane, pinsDown, standingPins);
+        }
+
+        static int ChosenLane(int chosenLane)
+        {
+            // CHECK IF THE ENTERED NUMBER IS VALID
+            Console.CursorVisible = true;
+            try
+            {
+                chosenLane = Int32.Parse(Console.ReadLine());
+            }
+
+            catch
+            {
+                Console.WriteLine("You have to enter a number.\nPress any key to try again.");
+                chosenLane = 666;
+                Console.CursorVisible = false;
+                Console.ReadKey();
+            }
+
+            return chosenLane;
+        }
+
+        static void DrawPins(char[] pins)
+        {
+            //DRAW THE STANDING PINS
+            Console.WriteLine($"{pins[1]}   {pins[8]}   {pins[0]}   {pins[7]}\n");
+            Console.WriteLine($"  {pins[2]}   {pins[9]}   {pins[6]}\n");
+            Console.WriteLine($"    {pins[3]}   {pins[5]}\n");
+            Console.WriteLine($"      {pins[4]}\n");
+        }
+
+        static void Interface(int rounds, string roll, char[] pins, bool end)
+        {
+            // DRAWS THE INTERFACE
+            Console.Clear();
+            Console.WriteLine($"Round: {rounds}/10");
+            Console.WriteLine($"{roll} roll\n");
+            Console.WriteLine($"Current Pins:\n");
+            DrawPins(pins);
+            Console.WriteLine("^ ^ ^ ^ ^ ^ ^");
+            Console.Write("1 2 3 4 5 6 7");
+
+            if(end == false)
+                Console.WriteLine("\n\nEnter where to roll the ball (1-7)");
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> standingPins = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            char[] pinsNew = new char[10] { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O' };
+            int chosenLane = 666;
+            int firstRoll = 0;
+            int secondRoll = 0;
+            string[,] score = new string[10, 3];
+            int pinsDown;
+            for(int y = 0; y < 10; y++)
+                for(int x = 0; x < 3; x++)
+                    score[y, x] = " ";
+
+            int[] totalScore = new int[] { };
+
+
+            for(int rounds = 0; rounds < 10; rounds++)
+            {
+                pinsDown = 0;
+                if(rounds < 9)
+                {
+
+                    int roll = 1;
+                    char[] pins = new char[10] { 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O' };
+
+                    retryFirst:
+                    Interface(rounds+1, "First", pins, false);
+                    Console.SetCursorPosition(0, 19);
+                    ScoreSheet(score);
+                    Console.SetCursorPosition(0, 17);
+                    chosenLane = ChosenLane(chosenLane);
+                    if(chosenLane == 666)
+                        goto retryFirst;
+
+                    else if(0 < chosenLane && chosenLane < 8)
+                    {
+                        Path(pins, chosenLane, pinsDown, standingPins);
+                        FirstRoll(score, pinsDown, rounds, firstRoll, standingPins);
+                    }
+
+                    else
+                        goto retryFirst;
+
+                    List<int> standingPinsCopy = new List<int>(standingPins);
+                    roll++;
+                    retrySecond:
+                    Interface(rounds+1, "Second", pins, false);
+                    Console.SetCursorPosition(0, 19);
+                    ScoreSheet(score);
+                    Console.SetCursorPosition(0, 17);
+                    chosenLane = ChosenLane(chosenLane);
+                    if(chosenLane == 666)
+                        goto retrySecond;
+
+                    else if(0 < chosenLane && chosenLane < 8)
+                    {
+                        Path(pins, chosenLane, pinsDown, standingPins);
+                        SecondRoll(score, pinsDown, rounds, firstRoll, secondRoll, standingPins, standingPinsCopy);
+                    }
+
+                    else
+                        goto retrySecond;
+
+                    Interface(rounds+1, "Second", pins, true);
+                    Console.SetCursorPosition(0, 19);
+
+                    ScoreSheet(score);
+                    Console.SetCursorPosition(0, 17);
+
+
+                    {
+                        Console.SetCursorPosition(0, 16);
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                    }
+
+
+
+
+                }
+
+
+                else
+                {
+                    /*
+
+     if(rounds == 10)
+     {
+         roll++;
+         retryThird:
+         Interface(rounds, "Third", pins, false);
+         Console.SetCursorPosition(0, 19);
+         ScoreSheet(score);
+         Console.SetCursorPosition(0, 17);
+         chosenLane = ChosenLane(chosenLane);
+         if(chosenLane == 666)
+             goto retryThird;
+
+         else if(0 < chosenLane && chosenLane < 8)
+             Path(pins, chosenLane, pinsLeft);
+
+         else
+             goto retryThird;
+     }
+     */
+                    // else
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
--- Week 5/Day 2 Mission 1 Full bowling score sheet/Program.cs
+++ Week 5/Day 2 Mission 1 Full bowling score sheet/Program.cs
@@ -5,286 +5,11 @@ namespace list_tutorial
 {
     class Program
     {
-        static object First(List<int> standingPinsCopy)
-        {
-            if(standingPinsCopy.Count == 0)
-            {
-                return "X";
-            }
-
-            else if(standingPinsCopy.Count == 10)
-            {
-                return "-";
-            }
-
-            else
-            {
-                return (10-standingPinsCopy.Count);
-            }
-        }
-
-        static object Second(List<int> standingPins,List<int> standingPinsCopy)
-        {
-            if(standingPins.Count == 0)
-            {
-                return "/";
-            }
-
-            else if(standingPinsCopy.Count == 10)
-            {
-                return "-";
-            }
-
-            else if(standingPinsCopy.Count == standingPins.Count)
-            {
-                return "-";
-            }
-
-            else
-            {
-                return standingPinsCopy.Count - standingPins.Count;
-            }
-        }
-
-        static void Path(List<int> standingPins,int number)
-        {
-            var rnd = new Random();
-            int path = rnd.Next(0,101);
-
-            if(path < 33)
-            {
-                number += 1;
-            }
-
-            if(34 < path && path > 66)
-            {
-                number -= 1;
-            }
-            KnockPinOnPath(standingPins,number);
-        }
-
-        static void KnockPinOnPath(List<int> standingPins,int number)
-        {
-            if(number == 1)
-            {
-                if(standingPins.Contains(7))
-                {
-                    standingPins.Remove(7);
-                }
-
-                else
-                {
-                    ;
-                }
-            }
-
-            if(number == 2)
-            {
-                if(standingPins.Contains(4))
-                {
-                    standingPins.Remove(4);
-                    Path(standingPins,number);
-                }
-
-                else
-                {
-                    ;
-                }
-            }
-
-            if(number == 3)
-            {
-                if(standingPins.Contains(2))
-                {
-                    standingPins.Remove(2);
-                    Path(standingPins,number);
-                }
-
-                else if(standingPins.Contains(8))
-                {
-                    standingPins.Remove(8);
-                    Path(standingPins,number);
-                }
-
-                else
-                {
-                    ;
-                }
-            }
-
-            if(number == 4)
-            {
-                if(standingPins.Contains(1))
-                {
-                    standingPins.Remove(1);
-                    Path(standingPins,number);
-                }
-
-                else if(standingPins.Contains(5))
-                {
-                    standingPins.Remove(5);
-                    Path(standingPins,number);
-                }
-
-                else
-                {
-                    ;
-                }
-            }
-
-            if(number == 5)
-            {
-                if(standingPins.Contains(3))
-                {
-                    standingPins.Remove(3);
-                    Path(standingPins,number);
-                }
-
-                else if(standingPins.Contains(9))
-                {
-                    standingPins.Remove(9);
-                    Path(standingPins,number);
-                }
-
-                else
-                {
-                    ;
-                }
-            }
-
-            if(number == 6)
-            {
-                if(standingPins.Contains(6))
-                {
-                    standingPins.Remove(6);
-                    Path(standingPins,number);
-                }
-            }
-
-            if(number == 7)
-            {
-                if(standingPins.Contains(10))
-                {
-                    standingPins.Remove(10);
-                }
-            }
-
-            else
-            {
-                ;
-            }
-        }
-
-
-        static void Pins(List<int> standingPins)
-        {
-            // 7
-            if(standingPins.Contains(7))
-            {
-                Console.Write("O   ");
-            }
-            else
-            {
-                Console.Write("    ");
-            }
-
-            // 8
-            if(standingPins.Contains(8))
-            {
-                Console.Write("O   ");
-            }
-            else
-            {
-                Console.Write("    ");
-            }
-
-            // 9
-            if(standingPins.Contains(9))
-            {
-                Console.Write("O   ");
-            }
-            else
-            {
-                Console.Write("    ");
-            }
-
-            // 10
-            if(standingPins.Contains(10))
-            {
-                Console.WriteLine("O\n");
-            }
-            else
-            {
-                Console.WriteLine(" \n");
-            }
-
-            // 4
-            if(standingPins.Contains(4))
-            {
-                Console.Write("  O   ");
-            }
-            else
-            {
-                Console.Write("      ");
-            }
-
-            // 5
-            if(standingPins.Contains(5))
-            {
-                Console.Write("O   ");
-            }
-            else
-            {
-                Console.Write("    ");
-            }
-
-            // 6
-            if(standingPins.Contains(6))
-            {
-                Console.WriteLine("O\n");
-            }
-            else
-            {
-                Console.WriteLine(" \n");
-            }
-
-            // 2
-            if(standingPins.Contains(2))
-            {
-                Console.Write("    O   ");
-            }
-            else
-            {
-                Console.Write("        ");
-            }
-
-            // 3
-            if(standingPins.Contains(3))
-            {
-                Console.WriteLine("O\n");
-            }
-            else
-            {
-                Console.WriteLine("\n");
-            }
-
-            // 1
-            if(standingPins.Contains(1))
-            {
-                Console.WriteLine("      O\n");
-            }
-            else
-            {
-                Console.WriteLine("       \n");
-            }
-
-        }
-
         static void Holder()
-        { 
-          int[][] pins = new int[10][];
+        {
+            int[][] pins = new int[10][];
             // DRAWING THE ROLLS OUT
-            for(int i = 0;i < 10;i++)
+            for(int i = 0; i < 10; i++)
             {
                 Console.WriteLine($"FRAME {i+1}");
                 Console.Write($"Roll 1: ");
@@ -367,16 +92,16 @@ namespace list_tutorial
                     Console.WriteLine(pins[i][0]);
 
                 Console.Write("Points gained: ");
-               // Console.WriteLine(pointsGained[i]);
+                // Console.WriteLine(pointsGained[i]);
 
 
                 Console.Write("Score: ");
-               // Console.WriteLine(frameScores[i]);
+                // Console.WriteLine(frameScores[i]);
 
 
                 Console.WriteLine($"\n");
 
-            }   
+            }
         }
 
         static void ScoreSheet()
@@ -385,7 +110,7 @@ namespace list_tutorial
             Console.ReadKey();
             Console.Clear();
 
-            for(int frame = 1;frame < 11;frame++)
+            for(int frame = 1; frame < 11; frame++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.WriteLine(
@@ -395,11 +120,11 @@ namespace list_tutorial
                     "│     │     │     │     │     │     │     │     │     │       │\n"+
                     "└─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴───────┘");
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.SetCursorPosition(0,0);
+                Console.SetCursorPosition(0, 0);
                 Console.Write("┌");
 
                 //DRAW FRAME BY FRAME
-                for(int i = 0;i < frame;i++)
+                for(int i = 0; i < frame; i++)
                 {
                     if(i == 9)
                         Console.Write("─┬─┬─┬─┐");
@@ -413,7 +138,7 @@ namespace list_tutorial
 
                 Console.Write("\n│");
 
-                for(int i = 0;i < frame;i++)
+                for(int i = 0; i < frame; i++)
                 {
                     Console.Write(" │");
                     if(pins[i][0] == 10)
@@ -492,7 +217,7 @@ namespace list_tutorial
 
                 Console.Write("\n│");
 
-                for(int i = 0;i < frame;i++)
+                for(int i = 0; i < frame; i++)
                 {
                     if(i == 9)
                         Console.Write(" └─┴─┴─┤");
@@ -506,7 +231,7 @@ namespace list_tutorial
 
 
 
-                for(int i = 0;i < frame;i++)
+                for(int i = 0; i < frame; i++)
                 {
                     if(pins[frame-1][0] == 10 && i != 9)
                     {
@@ -523,19 +248,19 @@ namespace list_tutorial
                         if(i == 9)
                         {
                             const int AlignmentLast = 5;
-                           // Console.Write($"{frameScores[i],AlignmentLast}  │");
+                            // Console.Write($"{frameScores[i],AlignmentLast}  │");
                         }
 
 
-                     //   else
-                           // Console.Write($"{frameScores[i],Alignment} │");
+                        //   else
+                        // Console.Write($"{frameScores[i],Alignment} │");
                     }
 
                 }
 
                 Console.Write("\n└");
 
-                for(int i = 0;i < frame;i++)
+                for(int i = 0; i < frame; i++)
                 {
                     if(i == 9)
                         Console.Write("───────┘");
@@ -561,12 +286,12 @@ namespace list_tutorial
             // CALCULATING THE ROLLS
             var Random = new Random();
             int[][] pins = new int[10][];
-            for(int i = 0;i < 10;i++)
+            for(int i = 0; i < 10; i++)
             {
                 // THE NINE NORMAL ROUNDS
                 if(i < 9)
                 {
-                    int first = Random.Next(0,11);
+                    int first = Random.Next(0, 11);
                     if(first == 10)
                     {
                         pins[i] = new int[] { first };
@@ -574,51 +299,51 @@ namespace list_tutorial
 
                     else
                     {
-                        int second = Random.Next(0,11-first);
-                        pins[i] = new int[] { first,second };
+                        int second = Random.Next(0, 11-first);
+                        pins[i] = new int[] { first, second };
                     }
 
                 }
                 // THE TENTH SPECIAL ROUND
                 else
                 {
-                    int first = Random.Next(0,11);
+                    int first = Random.Next(0, 11);
                     int second;
                     int third;
 
                     if(first == 10)
                     {
-                        second = Random.Next(0,11);
+                        second = Random.Next(0, 11);
                     }
 
                     else
                     {
-                        second = Random.Next(0,11-first);
+                        second = Random.Next(0, 11-first);
                     }
 
                     if(first == 10 || second == 10 || first + second == 10)
                     {
                         if(second == 10)
                         {
-                            third = Random.Next(0,11);
+                            third = Random.Next(0, 11);
                         }
 
                         else
                         {
-                            third = Random.Next(0,11-second);
+                            third = Random.Next(0, 11-second);
                         }
-                        pins[i] = new int[] { first,second,third };
+                        pins[i] = new int[] { first, second, third };
                     }
 
                     else
-                        pins[i] = new int[] { first,second };
+                        pins[i] = new int[] { first, second };
                 }
             }
 
             // CALCULATING SCORE BASED ON ROLLS
             int[] pointsGained = new int[10];
             int[] frameScores = new int[10];
-            for(int i = 1;i < 11;i++)
+            for(int i = 1; i < 11; i++)
             {
                 if(i < 9)
                 {
@@ -659,7 +384,7 @@ namespace list_tutorial
 
             }
 
-            for(int i = 0;i < 10;i++)
+            for(int i = 0; i < 10; i++)
             {
                 if(i == 0)
                     frameScores[0] = pointsGained[0];
@@ -672,110 +397,6 @@ namespace list_tutorial
             }
 
         }
-
-        static void Interface(int rounds, string round, List<int> standingPins)
-        {
-                Console.Clear();
-                Console.WriteLine($"Round: {rounds}/10\n");
-                Console.WriteLine($"{round} Roll");
-                Console.WriteLine($"Current Pins:\n");
-                Pins(standingPins);
-                Console.WriteLine("^ ^ ^ ^ ^ ^ ^");
-                Console.WriteLine("1 2 3 4 5 6 7");
-                Console.WriteLine("\n\nEnter where to roll the ball (1-7)");
-        }
-
-        static void Main(string[] args)
-        {
-            for(int rounds = 1;rounds < 11;rounds++)
-            {
-                var standingPins = new List<int> { 1,2,3,4,5,6,7,8,9,10 };
-                
-                // STARTING
-                retryFirst:
-
-                Interface(rounds, "First", standingPins);
-
-                // FIRST ROLL
-                var firstInput = Console.ReadLine();
-                int first;
-                bool firstTest = Int32.TryParse(firstInput,out first);
-
-                if(firstTest == true)
-                    first = Int32.Parse(firstInput);
-
-                if(0 < first && first < 8)
-                    Path(standingPins,first);
-
-                else
-                    goto retryFirst;
-
-                retrySecond:
-                Interface(rounds, "Second", standingPins);
-                var standingPinsCopy = new List<int>(standingPins);
-
-                
-
-                if(standingPins.Count == 0 && rounds == 9)
-                {
-                    standingPins = new List<int> { 1,2,3,4,5,6,7,8,9,10 };
-                    standingPins.Clear();
-                    standingPinsCopy.Clear();   
-                }
-
-                else if(standingPins.Count == 0)
-                {
-                    goto skip;
-                }
-
-                // SECOND ROLL
-                
-                var secondInput = Console.ReadLine();
-                int second;
-                bool secondTest = Int32.TryParse(secondInput,out second);
-
-                if(secondTest == true)
-                    second = Int32.Parse(secondInput);
-
-                if(0 < second && second < 8)
-                    Path(standingPins,second);
-
-                else
-                    goto retrySecond;
-
-                Path(standingPins,second);
-                Console.Clear();
-
-                Interface(rounds, "Second", standingPins);
-
-
-                if(standingPins.Count == 0 && rounds == 9)
-                {
-                    standingPins = new List<int> { 1,2,3,4,5,6,7,8,9,10 };
-                    standingPins.Clear();
-                    standingPinsCopy.Clear();
-
-                }
-
-                if(rounds == 9)
-                {
-
-
-                }
-                Interface(rounds, "Third", standingPins);
-
-
-                skip:
-
-
-                Console.WriteLine("\nPress any key to continue:");
-                Console.ReadKey();
-                Console.Clear();
-                standingPins.Clear();
-                standingPinsCopy.Clear();
-
-            }
- 
         }
 
     }
