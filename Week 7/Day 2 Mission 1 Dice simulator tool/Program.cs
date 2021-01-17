b230284923f1ee7c7200fef87cbd22e1ed1e5bd6
hllda <60630245+hllda@users.noreply.github.com>
2021-01-17 23:24:36 +0100

    last

--- Week 7/Day 2 Mission 1 Dice simulator tool/Program.cs
+++ Week 7/Day 2 Mission 1 Dice simulator tool/Program.cs
@@ -1,12 +1,313 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
 namespace Day_2_Mission_1_Dice_simulator_tool
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.White;
+            string options = "";
+            string diceNotation = "";
+
+            do
+            {
+                retry:
+                Console.WriteLine("DICE SIMULATOR\n");
+
+                Console.WriteLine("Enter dice roll in standard dice notation:");
+                if(options != "r")
+                    diceNotation = Console.ReadLine();
+                Console.WriteLine();
+
+                if(IsStandardDiceNotation(diceNotation) == true)
+                {
+                    Console.WriteLine($"Simulating...\n");
+                    Console.Write($"\nYou rolled: {DiceRoll(diceNotation)}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{diceNotation} is not in standard dice notation.");
+                    Console.WriteLine("Press any key to try again.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto retry;
+                }
+
+                Console.WriteLine($"\n");
+                Console.WriteLine("Do you want to (r)epeat, enter a (n)ew roll, or (q)uit?");
+                options = Console.ReadLine().ToLower();
+
+                if(options != "r" && options != "n")
+                {
+                    break;
+                }
+                Console.Clear();
+            } while(options != "q"); 
+        }
+
+        static bool IsStandardDiceNotation(string diceNotation)
+        {
+            string pattern = @"^([1-9]|\d\d)?d(\d+)([+|-]*\d*)$";
+
+            if(Regex.IsMatch(diceNotation, pattern))
+                return true;
+
+            else
+                return false;
+        }
+
+        static int DiceRoll(string diceNotation)
+        {
+            // INITIATING STANDARD VALUES
+            int numberRolls = 1;
+            int diceSides = 1;
+            int fixedBonus = 0;
+
+            string pattern = @"^([1-9]|\d\d)?d(\d+)([+|-]*\d*)$";
+            Match matches = Regex.Match(diceNotation, pattern);
+
+            try
+            {
+                numberRolls = Int32.Parse(matches.Groups[1].ToString());
+            }
+            catch { }
+
+            try
+            {
+                diceSides = Int32.Parse(matches.Groups[2].ToString());
+            }
+            catch { }
+
+            try
+            {
+                fixedBonus = Int32.Parse(matches.Groups[3].ToString());
+            }
+            catch { }
+
+            return DiceRoll(numberRolls, diceSides, fixedBonus);
+        }
+
+        static int DiceRoll(int numberRolls, int diceSides, int fixedBonus)
+        {
+            var random = new Random();
+            int roll = 0;
+            for(int i = 0; i < numberRolls; i++)
+            {
+                int rnd = random.Next(1, diceSides + 1);
+                roll += rnd;
+                Console.WriteLine(Art(numberRolls, diceSides, fixedBonus, rnd, i));
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            int diceRoll = roll + fixedBonus;
+            return diceRoll;
+        }
+
+        static string OrdinalNumber(int x)
+        {
+            int y = x % 10;
+            if(x < 10)
+            {
+                if(y == 1)
+                {
+                    return "st";
+                }
+
+                else if(y == 2)
+                {
+                    return "nd";
+                }
+
+                else if(y == 3)
+                {
+                    return "rd";
+                }
+
+                else
+                {
+                    return "th";
+                }
+            }
+
+            else
+            {
+                int z = (x / 10) % 10;
+                if(z == 1)
+                {
+                    return "th";
+                }
+
+                else
+                {
+                    if(y == 1)
+                    {
+                        return "st";
+                    }
+
+                    else if(y == 2)
+                    {
+                        return "nd";
+                    }
+
+                    else if(y == 3)
+                    {
+                        return "rd";
+                    }
+
+                    else
+                    {
+                        return "th";
+                    }
+                }
+            }
+        }
+
+        static string Art(int numberRolls, int diceSides, int fixedBonus, int rnd, int i)
+        {
+            {
+                // TWO SIDED DICE, AKA COIN TOSS
+                if(diceSides == 2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    const int align2 = 3;
+                    return
+                        $"\n  ___" +
+                        $"\n /   \\" +
+                        $"\n|{rnd,align2}  |" +
+                        $"\n \\___/";
+                }
+
+                // FOUR SIDED DICE
+                else if(diceSides == 4)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    const int align4 = -2;
+                    return
+                           $"\n  /\\." +
+                           $"\n /{rnd,align4}\\'." +
+                           $"\n/____\\/ \n";
+                }
+
+                // SIX SIDED STANDARD DICE
+                else if(diceSides == 6)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if(rnd == 1)
+                        return
+                            $"\n┌───────┐" +
+                            $"\n│       │" +
+                            $"\n│   O   │" +
+                            $"\n│       │" +
+                            $"\n└───────┘";
+
+                    if(rnd == 2)
+                        return
+                            $"\n┌───────┐" +
+                            $"\n│ O     │" +
+                            $"\n│       │" +
+                            $"\n│     O │" +
+                            $"\n└───────┘";
+
+                    if(rnd == 3)
+                        return
+                            $"\n┌───────┐" +
+                            $"\n│ O     │" +
+                            $"\n│   O   │" +
+                            $"\n│     O │" +
+                            $"\n└───────┘";
+
+                    if(rnd == 4)
+                        return
+                            $"\n┌───────┐" +
+                            $"\n│ O   O │" +
+                            $"\n│       │" +
+                            $"\n│ O   O │" +
+                            $"\n└───────┘";
+
+                    if(rnd == 5)
+                        return
+                            $"\n┌───────┐" +
+                            $"\n│ O   O │" +
+                            $"\n│   O   │" +
+                            $"\n│ O   O │" +
+                            $"\n└───────┘";
+
+                    else
+                        return
+                            $"\n┌───────┐" +
+                            $"\n│ O   O │" +
+                            $"\n│ O   O │" +
+                            $"\n│ O   O │" +
+                            $"\n└───────┘";
+                }
+
+                // EIGHT SIDED DICE
+                else if(diceSides == 8)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    const int align8 = -2;
+                    return
+                    $"\n   /\\." +
+                    $"\n  /  \\'." +
+                    $"\n / {rnd,align8} \\ '." +
+                    $"\n/______\\'/" +
+                    $"\n'.     // " +
+                    $"\n  '.  /'" +
+                    $"\n    '/";
+                }
+
+                // TEN SIDED DICE
+                else if(diceSides == 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    const int align10 = 3;
+                    return
+                    $"\n      ." +
+                    $"\n     / \\  " +
+                    $"\n   ./   \\. " +
+                    $"\n  // {rnd,align10} \\\\" +
+                    $"\n /.^.___.^.\\" +
+                    $"\n '.   |   .'" +
+                    $"\n   '. | .'      "+
+                    $"\n     '.'        ";
+                }
+
+                // TWELVE SIDED DICE
+                else if(diceSides == 12)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    const int align12 = 2;
+                    return
+                    $"\n     ____" +
+                    $"\n   .'  | '." +
+                    $"\n  /'. _| .'\\" +
+                    $"\n  |  /{rnd,align12}\\  |" +
+                    $"\n  \\ /\\__/\\ /" +
+                    $"\n   '.____.'";
+                }
+
+                // TWENTY SIDED DICE
+                else if(diceSides == 20)
+                {                  
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    const int align20 = 2;
+                    return
+                    $"\n     _____    " +
+                    $"\n   .'  |  '." +
+                    $"\n  /__ /\\ ___\\     " +
+                    $"\n |   /{rnd,align20}\\    | " +
+                    $"\n |'./____\\ .'| " +
+                    $"\n  \\/ \\  / \\ /       " +
+                    $"\n   '._\\/__.'          ";
+                }
+
+                // THE DICE NOTATIONS THAT DO NOT HAVE ART MADE
+                else
+                {
+                    return $"{i+1}{OrdinalNumber(i+1)} roll: {rnd}";
+                }
+            }
         }
     }
 }
--- Week 7/Day 3-5 Mission 1 Monster Manual with search/Program.cs
+++ Week 7/Day 3-5 Mission 1 Monster Manual with search/Program.cs
@@ -1,12 +1,113 @@
 ﻿using System;
-
-namespace Day_3_5_Mission_1_Monster_Manual_with_search
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace  Day_3_5_Mission_1_Monster_Manual_with_search
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"D:\Projects\TheIndieQuest\Week 7\Day 3-5 Mission 1 Monster Manual with search\MonsterManual.txt";
+            string manual = File.ReadAllText(path);
+
+            String[] monster = manual.Split("\n\n");
+
+            var namesByAlignment = new List<string>[3,3];
+
+            var namesOfUnaligned = new List<string>();
+            var namesOfAnyAlignment = new List<string>();
+            var namesOfSpecialCases = new List<string>();
+
+            for(int y = 0; y < 3; y++)
+                for(int x = 0; x < 3; x++)
+                    namesByAlignment[y,x] = new List<string>();
+
+            var xLabel = new[] { "lawful","neutral","chaotic" };
+            var yLabel = new[] { "good","neutral","evil" };
+            
+
+            for(int i = 0; i < monster.Length; i++)
+            {
+                String[] info = monster[i].Split("\n");
+
+                if(Regex.IsMatch(info[1],"(, ?(lawful|neutral|chaotic)? (good|neutral|evil))$"))
+                {
+
+                    for(int y = 0; y < 3; y++)
+                    {
+                        for(int x = 0; x < 3; x++)
+                        {
+                            string pattern = $"(, ?({xLabel[x]})? ({yLabel[y]}))";
+
+                            if(Regex.IsMatch(info[1],pattern))
+                            {
+                                namesByAlignment[y,x].Add(info[0]);
+                            }
+                        }
+                    }
+                }
+
+                else
+                {
+                    if(Regex.IsMatch(info[1],"(, unaligned)"))
+                    {
+                        namesOfUnaligned.Add(info[0]);
+                        continue;
+                    }
+
+                    else if(Regex.IsMatch(info[1],"(, any alignment)"))
+                    {
+                        namesOfAnyAlignment.Add(info[0]);
+                        continue;
+                    }
+
+                    else
+                    {
+                        string[] special = info[1].Split(", ");
+
+
+                        namesOfSpecialCases.Add($"{info[0]} ({special[1]})");
+                        continue;
+                    }
+                }
+            }
+
+            for(int y = 0; y < 3; y++)
+            {
+                for(int x = 0; x < 3; x++)
+                {   
+                    if(x == 1 && y == 1) Console.WriteLine($"Monsters with alignment true {yLabel[y]} are:");
+                    else Console.WriteLine($"Monsters with alignment {xLabel[x]} {yLabel[y]} are:");
+                    foreach(string thing in namesByAlignment[y,x])
+                    {
+                        Console.WriteLine(thing);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("Unaligned monsters are: ");
+            foreach(string thing in namesOfUnaligned)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Monsters which can be of any alignment are: ");
+            foreach(string thing in namesOfAnyAlignment)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Monsters with special cases are: ");
+            foreach(string thing in namesOfSpecialCases)
+            {
+                Console.WriteLine(thing);
+            }
         }
     }
 }
